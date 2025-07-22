63029163993e76cea03f7c7a0ec7d171e42797a0
assasin773 <mullarhudson6@gmail.com>
2025-07-22 10:51:38 +0300

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,14 @@
+Const settings ={
+  Packname:"MULLAR-NFS',
+    Author:",
+  BotName:"*MULLAR-NFS *☠💀",
+  BotOwner:'Mullar',//
+  Ownernumber:254100594249
+giphyApiKey:YJkFqTOAJLf71pqEYr3PpHrlzzfnUvWT',
+  Command mode:"private",
+  Description:This is a bot for managing group commands and automating tasks",
+  Version:"2.0.6",
+  };
+
+module.exports=settings.
+  
