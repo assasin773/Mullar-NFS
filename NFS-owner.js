6a346e034a497100b1ee8165fcfc1604b610f6db
assasin773 <mullarhudson6@gmail.com>
2025-07-22 15:01:20 +0300

    Update NFS-owner.js

--- NFS-owner.js
+++ NFS-owner.js
@@ -3,7 +3,7 @@ const settings = require('../settings');
 async function ownerCommand(sock, chatId) {
     const vcard = `
 BEGIN:VCARD
-VERSION:3.0
+VERSION 2.6.0
 FN:${settings.botOwner}
 TEL;waid=${settings.ownerNumber}:${settings.ownerNumber}
 END:VCARD
